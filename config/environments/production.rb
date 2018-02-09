5060f3909e8b0c4bd51b7e45b9d96e6406b481c3
Bruno Calmels <bruno.calmels@macherit.com>
2018-02-09 15:05:15 -0300

    feat(emails): Configuramos usuarios y mails.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ClubIdea_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Para Heroku
+  config.action_mailer.default_url_options = { host: 'https://clubemprendedor.herokuapp.com' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
