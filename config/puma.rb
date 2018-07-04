1332d689f224a16b8dd598b256e9ceeb2dc1ef0b
Bruno Calmels <bruno.calmels@macherit.com>
2018-07-04 15:33:09 -0300

    chore(puma config): Toma config de Puma de Rails5.2Template

--- config/puma.rb
+++ config/puma.rb
@@ -15,6 +15,23 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+workers_count = ENV.fetch("WEB_CONCURRENCY") { 1 }.to_i
+
+if workers_count > 1
+  workers workers_count
+
+  preload_app!
+
+  before_fork do
+    ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
+  end
+
+  on_worker_boot do
+    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+  end
+end
+
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
