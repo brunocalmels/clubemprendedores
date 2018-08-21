14351adf8d42b8091a91f74d6fe48b3f2cd99482
Bruno Calmels <bruno.calmels@macherit.com>
2018-08-21 15:51:02 -0300

    feat(mails): Vuelve a usar mail de SendGrid.

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'clubemprendedor.herokuapp.com',
+  domain:               'club.adeneu.com.ar',
   user_name:            ENV["GMAIL_USERNAME"],
   password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
@@ -15,16 +15,11 @@ elsif Rails.env.production?
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  address:              'lxqz-hn2h.accessdomain.com',
-  port:                 465,
-  domain:               'clubemprendedor.adeneu.com',
-  user_name:            ENV["MAIL_ADENEU_USERNAME"],
-  password:             ENV["MAIL_ADENEU_PASSWORD"],
-  # address:              'smtp.sendgrid.net',
-  # port:                 587,
-  # domain:               'clubemprendedor.adeneu.com',
-  # user_name:            ENV["SENDGRID_USERNAME"],
-  # password:             ENV["SENDGRID_PASSWORD"],
+  address:              'smtp.sendgrid.net',
+  port:                 587,
+  domain:               'club.adeneu.com.ar',
+  user_name:            ENV["SENDGRID_USERNAME"],
+  password:             ENV["SENDGRID_PASSWORD"],
   authentication:       'plain',
   enable_starttls_auto: true
 }
