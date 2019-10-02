35e334bd3c453030e934e5aecdb95a6362f30989
Bruno Calmels <bruno.calmels@macherit.com>
2019-10-02 18:17:34 -0300

    feat: Exportación de reservas a Google Calendar y página de documentación-

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,5 +1,12 @@
 class InicioController < ApplicationController
+  before_action :assure_admin!, only: %i[docs]
+
+  # GET /
   def index
     @reservas = Reserva.all
+  end
+
+  # GET /docs
+  def docs
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   authenticated :user do
     resources :perfiles, only: %i[show], controller: "users/users"
     get "reservas/esperando_aprobacion", to: "reservas#esperando_aprobacion"
+    get "reservas/to_calendar", to: "reservas#to_calendar"
     resources :reservas
     resources :grupos, only: %i[index edit update]
     put "grupos/:id/add_user", to: "grupos#add_user"
+    get "docs", to: "inicio#docs"
   end
 end
